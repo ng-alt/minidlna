4c2c3a396a569466e7fbdca51a9976f3b70eacf0
ccc <cadappl@gmail.com>
2018-01-24 17:18:05 +0800

    Import R6300v2-V1.0.4.24_10.0.87

--- upnpsoap.c
+++ upnpsoap.c
@@ -1811,9 +1811,14 @@ SamsungSetBookmark(struct upnphttp * h, const char * action)
 	PosSecond = GetValueFromNameValueList(&data, "PosSecond");
 	if( ObjectID && PosSecond )
 	{
+/* PSV-2017-3056 Fix: Unauthenticated UPNP/SOAP Request to minidlnad */
+        int sec = atoi(PosSecond);
+		if ( sec < 30 )
+			sec = 0;
 		ret = sql_exec(db, "INSERT OR REPLACE into BOOKMARKS"
 		                   " VALUES "
-		                   "((select DETAIL_ID from OBJECTS where OBJECT_ID = '%q'), %q)", ObjectID, PosSecond);
+		                   "((select DETAIL_ID from OBJECTS where OBJECT_ID = '%q'), %d)", ObjectID, sec);
+/* PSV-2017-3056 Fix: Unauthenticated UPNP/SOAP Request to minidlnad */
 		if( ret != SQLITE_OK )
 			DPRINTF(E_WARN, L_METADATA, "Error setting bookmark %s on ObjectID='%s'\n", PosSecond, ObjectID);
 		BuildSendAndCloseSoapResp(h, resp, sizeof(resp)-1);
